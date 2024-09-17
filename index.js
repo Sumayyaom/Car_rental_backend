ba1bd581afef718e1a00f8c525db2134a7ec0151
Sumayya <sumayyaom18@gmail.com>
2024-09-17 22:10:56 +0530

    Seventeenth commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://car-rental-backend-gamma.vercel.app',
   credentials: true,
 }))
 app.use(express.json());
